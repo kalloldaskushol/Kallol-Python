515a3d96dd2214b25b34bd9193554a8014435757
Kallol Das Kushol <kallold70@gmail.com>
2024-03-22 20:59:58 +0600

    all files upload

---
+++ 1)print.py
@@ -0,0 +1,15 @@
+print("Kallol Das Kushol")
+print("My age is 21")
+#A string should be in double course("")
+#we have to use multiple print function to write in multip;e line
+
+print("Kallol Das Kushol" , ("age 21"))
+#We have to use Comma in the Middle of two ()
+
+print("23")
+print(23)
+print(35)
+print("23+35")
+print(23+35)
+#we can write numbers without or with doublecourse("")
+#but if we have to calculate sum then we must not use double course("")
---
+++ 5)Type of Token.py
@@ -0,0 +1,16 @@
+#Type of Token
+#Punctuators
+#Punctuators are symbols to organize sentence structure in Programming
+#() : Parentheses
+#{} : curly Braces
+#[] : square Bracket
+# # : Hash
+# = :Assignment Operator
+#Ogmented Operator =+ , -= , /= , //= , *=
+#There are two types of Typed language 
+#1)Implicit-we don't have to mention the type
+#2)Explicit-we haveto mention the type
+#Python is a implicit typed language.so we don't have to mention the type.Like
+#age = 23
+#Java is a Explict typed language so we have to mention the type.Like
+#int age = 23
