6936f287fa7e52b108c9396393d672df39cca7fe
Kallol Das Kushol <kallold70@gmail.com>
2024-03-23 23:58:25 +0600

    normal

---
+++ 7-Input.py
@@ -0,0 +1,6 @@
+#INPUT
+#String input
+name =input ("Enter Your NAME :")
+age = int(input("Enter Your AGE :"))
+price = float(input("Enter Your Price :"))
+print("My name is", name, "and i'm", age,"years old","i have" ,price)
---
+++ 8-Practice-1.py
@@ -0,0 +1,21 @@
+#Practice-1
+#state True or False
+"""
+1)/* is a symbol used in Python for single line comment- False
+2)2ndName is an invalid identifier in Python - True;a varible can't start with number but it can be start with underscore _
+3)** is a valid arithmetic operator in Python - True;its used to asign power 
+4)in is a logical operator in Python - False; Logical operator - NOT,AND,OR
+5)Variable declaration is IMPLICIT in Python - True;implicit means we don't have to tell the what kind of variable it is
+6)Consider the given expression: not True and False or True
+which of the following will be correct output if the given expression is evaluated
+a)True - answer
+b)False
+c)NONE
+d)NULL
+ANSWER:
+ not,and,or
+not 1 and 0 or 1
+0 and 0 or 1
+0 or 1
+1 = True
+"""
