f91e3be6b79d8d88065977d4ba87b1d4d0e34e05
Kallol Das Kushol <kallold70@gmail.com>
2024-03-24 22:55:53 +0600

    normal

--- 1-print.py
+++ 1-print.py
@@ -4,6 +4,12 @@ print("My age is 21")
 #we have to use multiple print function to write in multip;e line
 
 print("Kallol Das Kushol" , ("age 21"))
+print("Kallol Das Kushol" + ("age 21"))
+
+name= "kallol"
+age = 23
+#print(name + age) - a string and an integer cant be together with a PLUS(+)
+print(name , age)#it can be written by Comma
 #We have to use Comma in the Middle of two ()
 
 print("23")
---
+++ 9)Conditional_Statements.py
@@ -0,0 +1,19 @@
+"""
+CONDITIONAL STATEMENTS
+if-elif-else(SYNTAX-RULES OF WRITING)
+if(CONDITION) :
+    STATEMENT1 (after if-elif-else there should be a 4space gap which is called indentation)
+elif(CONDITION) :
+    STATEMENT2
+else:
+    STATEMENT
+"""
+light =input("What is the colour:")
+if(light == "red"):
+    print("STOP")
+elif(light == "green"):
+    print("GOOOOOO")
+elif(light == "yellow"):
+    print("LOOK")
+else:
+    print("SORRY! THE LIGHT IS BROKEN")
