22197cf16c16c8a24d2cb70375ca81b19557cf2a
Kallol Das Kushol <kallold70@gmail.com>
2024-04-05 14:06:48 +0600

    normal

--- 14-Operators.py
+++ 14-Operators.py
@@ -20,5 +20,30 @@ print(a == b) #False
 print(a != b) #True
 print(a >= b) #True
 print(a > b) #True
-print(a <= b) #True
-print(a > b) #True
+print(a <= b) #False
+print(a < b) #False
+
+#assignment operators(= += -= /= %= **=)
+num = 10
+num = num + 10 # num =10+10=20
+print("num = ", num)
+num += 10 #num = num +10 =20+10=30
+print("num += = ", num)
+num -= 5 #num =num - 5 = 30-5 =25
+print("num -= = ", num)
+num *= 5  #num = num * 5 = 25*5=125
+print("num *= = ", num)
+num /= 5 #num = num/5 = 125/5 =25
+print("num /= = ", num)
+num %= 5 #num = (num % 5) = 25%5=0
+print("num %= = ", num)
+num **=0 #num = 0 to the power 0
+print("num **= = ", num)
+
+#logical operators (NOT , AND , OR)
+a= 50
+b= 30
+print (not False)
+print(not (a > b))
+print(a and b)#When using the and operator, Python returns the second operand if the first operand is truthy. In this case
+print(a or b)#When using the or operator, Python returns the first operand if it is truthy
