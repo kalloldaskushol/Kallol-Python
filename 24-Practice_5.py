1cbaddc1409a7565afee2ba17516d89bc9df0c5b
Kallol Das Kushol <kallold70@gmail.com>
2024-04-15 14:09:23 +0600

    normal

---
+++ 24-Practice_5.py
@@ -0,0 +1,31 @@
+#Practice_5
+#WAP to check if a number is even or odd
+
+number = int(input("Enter the number ="))
+if(number % 2 == 0):
+    print("The number is even")#2,4,6,8....
+else:
+    print("The number is odd")#1,3,5,7,9....
+
+
+
+#WAP to find the greatest of 3 numbers entered by the user
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+c=int(input("enter the third number:"))
+
+if(a>b and a>b):
+    print("The first number is the largest")
+elif(b>a and b>c):
+    print("The second number is the largest")
+else:
+    print("The third number  is the largest")
+
+
+
+#WAP to check if a number is multiple of 7 or not
+Number = int(input("Enter the Number:"))
+if (Number % 7 == 0):
+    print("The Number is multiple of 7")
+else:
+    print("The Number is not multiple of 7")
