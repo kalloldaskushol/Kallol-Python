474855035f2d186ed6c344a123ce120a544fadf7
Kallol Das Kushol <kallold70@gmail.com>
2024-04-16 15:23:51 +0600

    normal

---
+++ 30)Practice_7.py
@@ -0,0 +1,11 @@
+#Practice_7
+#WAP to count the number of students with the grade A in the following tuple
+# ("C" , "D" , "A" , "A" , "B" , "B" , "A")
+grade =("C" , "D" , "A" , "A" , "B" , "B" , "A")
+print(grade.count("A"))
+
+#Store the tuple in a list and sort them in a list
+
+grade =["C" , "D" , "A" , "A" , "B" , "B" , "A"]
+grade.sort()
+print(grade)
