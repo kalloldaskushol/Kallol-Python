d58dbc01cc22ac75d00f7fd47d8c4369667447ea
Kallol Das Kushol <kallold70@gmail.com>
2024-06-23 23:13:51 +0600

    normal

--- 51)Practice_12.py
+++ 51)Practice_12.py
@@ -7,6 +7,52 @@ using java
 i like programming in Java.
 """
 
-with open("Practice.txt" , "w+") as f:
+with open("52)Practice.txt" , "w+") as f:
     f.write("Hi everyone\nwe are learning file I/O\nusing java ")
     f.write("\nI like programming in java")
+
+#WA function that replaces all occurrences of java to python
+
+def change_java_python():
+    with open("52)Practice.txt","r") as f:
+        data = f.read()
+    new_data = data.replace("java","Python")
+#1st we read the file and add a new veriable called new_data that uses replace function which will replace java to python
+    with open("52)Practice.txt","w") as f:
+        f.write(new_data)
+#we write the file and use write function which will add full new data to the file
+#the new_data consists full writing not only python
+
+change_java_python()
+
+#WA function which will  searcch if the word (learning) exists in the file or not
+
+def find_learning():
+    word = "learning"
+    with open("52)Practice.txt" , "r") as f:
+        data = f.read()
+        if(data.find(word) != -1):
+#If the substring is not found, find() returns -1.
+#In the context of find(), -1 explicitly means that the substring does not exist in the string being searched.
+            print("Found")
+        else:
+            print("Not Found")
+
+find_learning()
+
+#WAF to find in which line of the file does the word "learning" occurs first.Print -1 if word not found.
+
+def find_line_Numb():
+    word = "Python"
+    data = True
+    line_Num = 1
+    with open("52)Practice.txt" , "r") as f:
+        while (data):
+            data = f.readline()
+            if(word in data): #works like code_line 34 -> it means finding learing in data which is the readline
+                print(line_Num)
+                return
+            line_Num = line_Num + 1
+    return print("-1")
+
+find_line_Numb()
